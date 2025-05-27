215a5077a14a919622f0044adceb135854fd32a3
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-05-27 16:32:50 +0300

    Add messaging functionality with send, retrieve messages, and user sidebar endpoints

--- server/src/routes/messages.route.ts
+++ server/src/routes/messages.route.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import { protectedRoute } from "../middlewares/protectedRoute.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/messages.controller.js";
 
 const router = express.Router();
 
-router.get("/conversations", (req, res) => {
-  res.send("Conversations page");
-});
+router.get("/conversations", protectedRoute, getUsersForSidebar);
+
+router.post("/send/:id", protectedRoute, sendMessage);
+
+router.get("/:id", protectedRoute, getMessages);
 
 export default router;
